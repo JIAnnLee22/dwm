30a1cb28302201d278d52560affcad7f682c2b18
JIAnnLee22 <947807614@qq.com>
2022-05-01 23:36:04 +0800

    patch:fullgap

--- config.def.h
+++ config.def.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -10,7 +11,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int viewontag          = 1;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Sauce Code Pro:size=14" };
+static const char *fonts[]          = { "Sauce Code Pro Nerd Font Mono:size=14" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -40,6 +41,7 @@ static const Rule rules[] = {
 	/* class 		instance    title       			tags mask     isfloating   monitor */
 	{ "gimp-2.10", 		NULL,       "GNU Image Manipulation Program",   0,            0,           -1 },
 	{ "jetbrains-studio",   NULL,       "win0",     			0, 	      1,           -1 },
+	{ "jetbrains-studio",   NULL,  	    "Welcome to Android Studio",  	0, 	      1,           -1 },
 	{ "weixin", 		NULL, 	    NULL, 	  			0, 	      1,           -1 },
 	{ "telegram-desktop",   NULL, 	    NULL, 				0, 	      1,           -1 },
 };
@@ -73,6 +75,7 @@ static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "drun"};
+static const char *slockcmd[]  = { "slock", NULL, NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -99,7 +102,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	{ MODKEY, 	                XK_s, 	   togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY, 	                XK_o, 	   spawn,  	   {.v = slockcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)

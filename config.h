50fd11f7cc867f2111f6232214d14493c8b4729b
JIAnnLee22 <947807614@qq.com>
2022-07-18 14:29:38 +0800

    dmenu

--- config.h
+++ config.h
@@ -11,6 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
+static const int viewontag          = 1;     /* switch view on tag */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -32,12 +33,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class 		instance    title       			tags mask     isfloating   monitor */
-	{ "gimp-2.10", 		NULL,       "GNU Image Manipulation Program",   0,            0,           -1 },
-	{ "jetbrains-studio",   NULL,       "win0",     			0, 	      1,           -1 },
-	{ "jetbrains-studio",   NULL,  	    "Welcome to Android Studio",  	0, 	      1,           -1 },
-	{ "weixin", 		NULL, 	    NULL, 	  			0, 	      1,           -1 },
-	{ "telegram-desktop",   NULL, 	    NULL, 				0, 	      1,           -1 },
+	/* class              instance title tags mask isfloating monitor */
+	{ "gimp-2.10",        NULL,    NULL, 0,        0,         -1 },
+	{ "jetbrains-studio", NULL,    NULL, 0,        1,         -1 },
+	{ "weixin",           NULL,    NULL, 0,        1,         -1 },
+	{ "telegram-desktop", NULL,    NULL, 0,        1,         -1 },
 };
 
 /* layout(s) */
@@ -68,13 +68,17 @@ static const Layout layouts[] = {
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
-static const char *roficmd[]  = { "rofi", "-show", "drun"};
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *slockcmd[]  = { "slock", NULL, NULL};
+static const char *changebgcmd[]  = { "feh", "--randomize", "--bg-fill", "/home/jiannlee22/pictures", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
-	{ MODKEY, 	                XK_Return, spawn,          {.v = termcmd } },
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = changebgcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -84,7 +88,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_z, 	   zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,         		XK_q,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -99,8 +103,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	{ MODKEY, 	                XK_s, 	   togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY, 	                XK_o, 	   spawn,  	   {.v = slockcmd } },
+	{ MODKEY,                       XK_s,      togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_o,      spawn,          {.v = slockcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -125,8 +129,6 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 

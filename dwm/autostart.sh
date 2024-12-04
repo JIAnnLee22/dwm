25a2a42144a959f78799d86646892e92f460bd6f
lja <947807614@qq.com>
2024-12-04 16:00:31 +0800

    fix no filechooser

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+/usr/lib/xdg-desktop-portal-gtk &
 picom &
 feh --randomize --bg-fill ~/Pictures &
 slstatus &
--- hypr/portal.sh
+++ hypr/portal.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+sleep 1
+killall -e xdg-desktop-portal-hyprland
+killall xdg-desktop-portal
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal-gtk &
+
+
+
+
 # usage: $0 portal-name [enable/disable]
 # args:
 #     portal-name: the name of the .portal file in /usr/share/xdg-desktop-portal/portals/ without the extension
@@ -6,42 +17,42 @@
 #                              or to move it into $DISABLED_PORTAL_DIR to disable it. The portal will be toggled
 #                              if this argument is omitted.
 
-ENABLED_PORTAL_DIR="/usr/share/xdg-desktop-portal/portals"
-# needs to be created manually
-DISABLED_PORTAL_DIR="/usr/share/xdg-desktop-portal/disabled-portals"
-
-is_portal_enabled() {
-  [ -f "$ENABLED_PORTAL_DIR/$1.portal" ]
-}
-
-is_portal_disabled() {
-  [ -f "$DISABLED_PORTAL_DIR/$1.portal" ]
-}
-
-enable_portal() {
-  mv "$DISABLED_PORTAL_DIR/$1.portal" "$ENABLED_PORTAL_DIR/$1.portal"
-}
-
-disable_portal() {
-  mv "$ENABLED_PORTAL_DIR/$1.portal" "$DISABLED_PORTAL_DIR/$1.portal"
-}
-
-remove_disabled_portal() {
-  rm "$DISABLED_PORTAL_DIR/$1.portal"
-}
-
-PORTAL=$1
-ENABLE=${2:-$(is_portal_enabled "$PORTAL" && echo "disable" || echo "enable")}
-
-if [ "$ENABLE" = "enable" ]; then
-  if is_portal_enabled "$PORTAL"; then
-    # remove disabled portal if enabled portal exists as well,
-    # as the enabled portal is most likely a newly installed version
-    is_portal_disabled "$PORTAL" && remove_disabled_portal "$PORTAL"
-  else
-    is_portal_disabled "$PORTAL" && enable_portal "$PORTAL"
-  fi
-else
-  is_portal_enabled "$PORTAL" && disable_portal "$PORTAL"
-fi
-
+#ENABLED_PORTAL_DIR="/usr/share/xdg-desktop-portal/portals"
+## needs to be created manually
+#DISABLED_PORTAL_DIR="/usr/share/xdg-desktop-portal/disabled-portals"
+#
+#is_portal_enabled() {
+#  [ -f "$ENABLED_PORTAL_DIR/$1.portal" ]
+#}
+#
+#is_portal_disabled() {
+#  [ -f "$DISABLED_PORTAL_DIR/$1.portal" ]
+#}
+#
+#enable_portal() {
+#  mv "$DISABLED_PORTAL_DIR/$1.portal" "$ENABLED_PORTAL_DIR/$1.portal"
+#}
+#
+#disable_portal() {
+#  mv "$ENABLED_PORTAL_DIR/$1.portal" "$DISABLED_PORTAL_DIR/$1.portal"
+#}
+#
+#remove_disabled_portal() {
+#  rm "$DISABLED_PORTAL_DIR/$1.portal"
+#}
+#
+#PORTAL=$1
+#ENABLE=${2:-$(is_portal_enabled "$PORTAL" && echo "disable" || echo "enable")}
+#
+#if [ "$ENABLE" = "enable" ]; then
+#  if is_portal_enabled "$PORTAL"; then
+#    # remove disabled portal if enabled portal exists as well,
+#    # as the enabled portal is most likely a newly installed version
+#    is_portal_disabled "$PORTAL" && remove_disabled_portal "$PORTAL"
+#  else
+#    is_portal_disabled "$PORTAL" && enable_portal "$PORTAL"
+#  fi
+#else
+#  is_portal_enabled "$PORTAL" && disable_portal "$PORTAL"
+#fi
+#
